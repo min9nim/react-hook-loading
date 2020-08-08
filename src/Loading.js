8e412779e2f094b40e8f533a7cd4c554a9ff5dac
min9nim <min9nim@gmail.com>
2020-08-08 22:27:14 +0900

    convert css in js

--- src/Loading.js
+++ src/Loading.js
@@ -1,11 +1,33 @@
 import React from 'react'
-import styles from './Loading.module.css'
+// import styles from './Loading.module.css'
+
+const modalStyle = {
+  position: 'fixed',
+  left: '0',
+  top: '0',
+  width: '100%',
+  height: '100%',
+  opacity: '0.7',
+  backgroundColor: '#111',
+}
+const loadingStyle = {
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  'background-color': '#fff',
+  padding: '2px',
+  '-webkit-transform': 'translate(-50%, -50%)',
+  '-ms-transform': 'translate(-50%, -50%)',
+  '-moz-transform': 'translate(-50%, -50%)',
+  '-o-transform': 'translate(-50%, -50%)',
+  transform: 'translate(-50%, -50%)',
+}
 
 export default function Loading() {
   return (
     <React.Fragment>
-      <div className={styles.modal_div} />
-      <div className={styles.Loading}>Loading..</div>
+      <div style={modalStyle} />
+      <div style={loadingStyle}>Loading..</div>
     </React.Fragment>
   )
 }
