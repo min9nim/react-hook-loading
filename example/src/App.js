749ab16fcd0924c15714400641e5e1f5c7ba12f0
min9nim <min9nim@gmail.com>
2020-08-08 20:28:57 +0900

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-useloading'
+import 'react-useloading/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
